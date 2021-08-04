42c3ccfd2fe42a8b4c00f46e980555b3d8665d96
jvaldiviab <juancarlosvaldiviaberrios@gmail.com>
2021-08-04 01:15:11 -0500

    update 04 - 08

--- app/src/main/java/com/finals/project03/SplashActivity.java
+++ app/src/main/java/com/finals/project03/SplashActivity.java
@@ -2,13 +2,37 @@ package com.finals.project03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private  int SLEEP_TIMER = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
     }
+    private class LogoLauncher extends  Thread{
+        public  void run(){
+            try{
+                sleep(1000 * SLEEP_TIMER);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    }
+
 }
